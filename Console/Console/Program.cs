bf7390c56a9893960b036aba3680996b2e0a79fa
TamSchnow265 <TamSchnow265@proton.me>
2023-01-24 14:55:42 +0100

    a new app

---
+++ Console/Console/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading;
+using System.Security.Principal;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Loading...");
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.Write("\r{0}%", i);
+            Thread.Sleep(50);
+        }
+        Console.WriteLine("\nLoading completed!");
+
+        WindowsIdentity currentUser = WindowsIdentity.GetCurrent();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {currentUser.Name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+
+}
+
