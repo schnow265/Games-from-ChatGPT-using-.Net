531e7fb2c94311a779292ec0bc921b25d02d6f82
TamSchnow265 <TamSchnow265@proton.me>
2023-01-24 15:46:04 +0100

    some more additions

--- Console/Console/Program.cs
+++ Console/Console/Program.cs
@@ -2,11 +2,13 @@
 using System;
 using System.Threading;
 using System.Security.Principal;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Loading Screen
         Console.WriteLine("Loading...");
         for (int i = 0; i <= 100; i++)
         {
@@ -14,12 +16,40 @@ class Program
             Thread.Sleep(50);
         }
         Console.WriteLine("\nLoading completed!");
+        Thread.Sleep(60);
+        Console.Clear();
 
+        // Welcome
         WindowsIdentity currentUser = WindowsIdentity.GetCurrent();
         var currentDate = DateTime.Now;
         Console.WriteLine($"{Environment.NewLine}Hello, {currentUser.Name}, on {currentDate:d} at {currentDate:t}!");
-        Console.Write($"{Environment.NewLine}Press any key to exit...");
-        Console.ReadKey(true);
+        
+        // Input
+
+        Start:
+        Console.Write("Enter a command: ");
+        string input = Console.ReadLine();
+            
+            //help
+        if (input.ToLower() == "help")
+        {
+            Console.WriteLine("This is help.");
+            goto Start;
+        }
+            //exit
+        else if (input.ToLower() == "exit")
+        {
+            // Exit
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+            //invalider
+        else
+        {
+            Console.WriteLine($"Hey, {currentUser.Name}! Did you read help? Go and try it!");
+            goto Start;
+        }
+
     }
 
 }
